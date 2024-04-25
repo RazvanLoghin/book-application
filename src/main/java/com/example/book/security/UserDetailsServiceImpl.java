7c6a6b259f9c1fdb4be30acf28a06aaa35d3b211
RazvanLoghin <rloghin55@yahoo.com>
2024-04-25 15:09:48 +0300

    Add and configure security layer

---
+++ src/main/java/com/example/book/security/UserDetailsServiceImpl.java
@@ -0,0 +1,25 @@
+package com.example.book.security;
+
+import com.example.book.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import static java.lang.String.format;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String userEmail) throws UsernameNotFoundException {
+        return userRepository.findByEmail(userEmail)
+                .orElseThrow(() ->
+                        new UsernameNotFoundException(format("User with email %s not found", userEmail)));
+    }
+}
